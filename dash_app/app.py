67f7c7d1587d24ed683361f1d8f2ab3bdaf4ccee
jehalley <jeffhalley@gmail.com>
2019-10-02 10:17:38 -0700

    trying change in weekly average instead of monthly in query

--- dash_app/app.py
+++ dash_app/app.py
@@ -22,7 +22,7 @@ end_date_string = "2018-07-02"
 
 connection = psycopg2.connect(host='127.0.0.1', port=5431, user='jh', password='jh', dbname='word')
 #query = "SELECT topic, date, word, sub_freq_to_all_freq_ratio  FROM reddit_results WHERE topic = 'Basketball' AND '[2016-01-01, 2017-01-01]'::daterange @> date ORDER BY sub_freq_to_all_freq_ratio  DESC LIMIT 10;"
-query = "SELECT topic, date, word, count, freq_in_topic, sub_freq_to_all_freq_ratio, change_in_monthly_average FROM reddit_results_test WHERE topic = '" + initial_topic + "' AND '[" + start_date_string + ", " + end_date_string + "]'::daterange @> date ORDER BY change_in_monthly_average DESC;"
+query = "SELECT topic, date, word, count, freq_in_topic, sub_freq_to_all_freq_ratio, change_in_weekly_average FROM reddit_results_test WHERE topic = '" + initial_topic + "' AND '[" + start_date_string + ", " + end_date_string + "]'::daterange @> date;"
 cursor = connection.cursor()
 cursor.execute(query)
 data = cursor.fetchall()
@@ -186,7 +186,7 @@ def get_date_range(start_date, end_date):
      dash.dependencies.Input('chosen_date_range_string', 'children')])
 def update_table(topic, date_range):
     #global topic
-    query = "SELECT topic, date, word, count, freq_in_topic, sub_freq_to_all_freq_ratio, change_in_monthly_average FROM reddit_results_test WHERE topic = '" + topic + "' AND '[" + date_range + "]'::daterange @> date ORDER BY change_in_monthly_average;"
+    query = "SELECT topic, date, word, count, freq_in_topic, sub_freq_to_all_freq_ratio, change_in_weekly_average FROM reddit_results_test WHERE topic = '" + topic + "' AND '[" + date_range + "]'::daterange @> date;"
     cursor = connection.cursor()
     cursor.execute(query)
     data = cursor.fetchall()
