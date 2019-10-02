99def35600d5791c97308c48f1869c91bfba52cb
jehalley <jeffhalley@gmail.com>
2019-10-02 10:31:51 -0700

    trying to comment out the column specs

--- dash_app/app.py
+++ dash_app/app.py
@@ -101,24 +101,26 @@ app.layout = html.Div([
     html.Div(id='output-container-date-picker-range'),
     
     dash_table.DataTable(
-        id='datatable-interactivity',
-        columns=[
-            {"name": i, "id": i, "deletable": True, "selectable": True} for i in df.columns
-        ],
-        data=df.to_dict('records'),
-        editable=True,
-        filter_action="native",
-        sort_action="native",
-        sort_mode="multi",
-        column_selectable="single",
-        row_selectable="multi",
-        row_deletable=True,
-        selected_columns=[],
-        selected_rows=[],
-        page_action="native",
-        page_current= 0,
-        page_size= 10,
-    ),
+        id='datatable-interactivity'),
+            
+#        id='datatable-interactivity',
+#        columns=[
+#            {"name": i, "id": i, "deletable": True, "selectable": True} for i in df.columns
+#        ],
+#        data=df.to_dict('records'),
+#        editable=True,
+#        filter_action="native",
+#        sort_action="native",
+#        sort_mode="multi",
+#        column_selectable="single",
+#        row_selectable="multi",
+#        row_deletable=True,
+#        selected_columns=[],
+#        selected_rows=[],
+#        page_action="native",
+#        page_current= 0,
+#        page_size= 10,
+#    ),
     html.Div(id='datatable-interactivity-container'),
     
     dcc.Graph(id='graph'),
