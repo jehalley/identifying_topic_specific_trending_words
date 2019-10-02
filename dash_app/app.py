8c8e06ffe9a386256db02ac6ae1779ad23b37764
jehalley <jeffhalley@gmail.com>
2019-10-02 10:59:53 -0700

    going back cannot get table to update on single input

--- dash_app/app.py
+++ dash_app/app.py
@@ -21,7 +21,8 @@ start_date_string = "2018-07-01"
 end_date_string = "2018-07-02"
 
 connection = psycopg2.connect(host='127.0.0.1', port=5431, user='jh', password='jh', dbname='word')
-query = "SELECT topic, date, word, count, freq_in_topic, sub_freq_to_all_freq_ratio, change_in_weekly_average FROM reddit_results_test WHERE topic = '" + initial_topic + "' AND '[" + start_date_string + ", " + end_date_string + "]'::daterange @> date;"
+#query = "SELECT topic, date, word, sub_freq_to_all_freq_ratio  FROM reddit_results WHERE topic = 'Basketball' AND '[2016-01-01, 2017-01-01]'::daterange @> date ORDER BY sub_freq_to_all_freq_ratio  DESC LIMIT 10;"
+query = "SELECT topic, date, word, count, freq_in_topic, sub_freq_to_all_freq_ratio, change_in_monthly_average FROM reddit_results_test WHERE topic = '" + initial_topic + "' AND '[" + start_date_string + ", " + end_date_string + "]'::daterange @> date ORDER BY change_in_monthly_average DESC;"
 cursor = connection.cursor()
 cursor.execute(query)
 data = cursor.fetchall()
@@ -36,13 +37,11 @@ query_data = pd.DataFrame(data = data, columns=['topic',
 
 query_data['adjusted_change_in_freq'] = query_data['change_in_relevance']/query_data['freq_in_topic']
 average_relevance = pd.DataFrame(query_data.groupby('word')['topic_relevance'].mean())
-top_percentile_words = average_relevance[average_relevance.topic_relevance > average_relevance.topic_relevance.quantile(.20)]
+top_percentile_words = average_relevance[average_relevance.topic_relevance > average_relevance.topic_relevance.quantile(.80)]
 query_data = pd.merge(top_percentile_words, query_data, how='left', on = 'word')
 query_data = query_data.sort_values('adjusted_change_in_freq', ascending=False)
 df_with_extra_columns = query_data.head(10)
 df = df_with_extra_columns[['topic','word','topic_relevance_y','adjusted_change_in_freq']]
-df.columns = ['topic','word','topic_relevance','adjusted_change_in_freq']
-
 
 #this will be applied to the list of subreddits explained below
 def topic_to_options_dict(x):
@@ -174,21 +173,10 @@ def get_date_range(start_date, end_date):
     [dash.dependencies.Output('datatable-interactivity', 'data'),
      dash.dependencies.Output('query_results', 'children')],
     [dash.dependencies.Input('topic_from_pulldown', 'children'),
-     dash.dependencies.Input('my-date-picker-range', 'start_date'),
-     dash.dependencies.Input('my-date-picker-range', 'end_date')])
-def update_table(topic, start_date, end_date):
+     dash.dependencies.Input('chosen_date_range_string', 'children')])
+def update_table(topic, date_range):
     #global topic
-    if start_date is not None:
-        start_date = dt.strptime(start_date, '%Y-%m-%d')
-        start_date_string = start_date.strftime('%Y-%m-%d')
-        
-    if end_date is not None:
-        end_date = dt.strptime(end_date, '%Y-%m-%d')
-        end_date_string = end_date.strftime('%Y-%m-%d')
-    
-    date_range = start_date_string + ", " + end_date_string
-    
-    query = "SELECT topic, date, word, count, freq_in_topic, sub_freq_to_all_freq_ratio, change_in_weekly_average FROM reddit_results_test WHERE topic = '" + initial_topic + "' AND '[" + date_range + "]'::daterange @> date;"
+    query = "SELECT topic, date, word, count, freq_in_topic, sub_freq_to_all_freq_ratio, change_in_monthly_average FROM reddit_results_test WHERE topic = '" + topic + "' AND '[" + date_range + "]'::daterange @> date ORDER BY change_in_monthly_average;"
     cursor = connection.cursor()
     cursor.execute(query)
     data = cursor.fetchall()
@@ -203,13 +191,13 @@ def update_table(topic, start_date, end_date):
     
     query_data['adjusted_change_in_freq'] = query_data['change_in_relevance']/query_data['freq_in_topic']
     average_relevance = pd.DataFrame(query_data.groupby('word')['topic_relevance'].mean())
-    top_percentile_words = average_relevance[average_relevance.topic_relevance > average_relevance.topic_relevance.quantile(.2)]
+    top_percentile_words = average_relevance[average_relevance.topic_relevance > average_relevance.topic_relevance.quantile(.80)]
     query_data = pd.merge(top_percentile_words, query_data, how='left', on = 'word')
     query_data = query_data.sort_values('adjusted_change_in_freq', ascending=False)
     #query_data = query_data['topic', 'date', 'word', 'count', 'sub_freq_to_all_freq_ratio_y','change_in_rolling_average']
     df_with_extra_columns = query_data.head(10)
     df = df_with_extra_columns[['topic','word','topic_relevance_y','adjusted_change_in_freq']]
-    df.columns = ['topic','word','topic_relevance','adjusted_change_in_freq']
+    
     data=df.to_dict('records')
     return data, query_data.to_json(date_format='iso', orient='split')
 
